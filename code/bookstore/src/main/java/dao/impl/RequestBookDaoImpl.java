7f34de1e440e9ec63a27f91e5f6085063781f879
pc-PC\pc <710449015@qq.com>
2017-07-14 15:52:54 +0800

    RequestBook等相关配置文件

---
+++ code/bookstore/src/main/java/dao/impl/RequestBookDaoImpl.java
@@ -0,0 +1,35 @@
+package dao.impl;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import dao.RequestBookDao;
+import model.RequestBook;
+
+public class RequestBookDaoImpl extends HibernateDaoSupport implements RequestBookDao{
+
+	@Override
+	public Integer save(RequestBook requestBook) {
+		return (Integer)getHibernateTemplate().save(requestBook);
+	}
+
+	@Override
+	public void delete(RequestBook requestBook) {
+		getHibernateTemplate().delete(requestBook);
+	}
+
+	@Override
+	public void update(RequestBook requestBook) {
+		getHibernateTemplate().update(requestBook);
+	}
+
+	@Override
+	public List<RequestBook> getProcessedRequest(String userName) {
+		//List<RequestBook> RequestBookList = (List<RequestBook>)getHibernateTemplate().find("from requestBook as r where r.userName = ? and r.requestStatus != ?", userName,"waiting");
+		List<RequestBook> RequestBookList = (List<RequestBook>)getHibernateTemplate().find("from RequestBook as r where r.userName = ? and r.requestStatus != ?", userName,"waiting");
+
+		return RequestBookList;
+	}
+
+}
